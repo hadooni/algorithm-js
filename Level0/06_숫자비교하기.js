99678ef98ddc372314bea8c07301a60f458d6f95
hadooni <hayoung7259@gmail.com>
2024-08-07 09:03:06 +0900

    solve : 숫자 비교하기

---
+++ Level0/06_숫자비교하기.js
@@ -0,0 +1,14 @@
+// Solution 1
+function solution1(num1, num2) {
+  if (num1 === num2) {
+    result = 1;
+  } else {
+    result = -1;
+  }
+  return result;
+}
+
+// Solution 2
+function solution2(num1, num2) {
+  return num1 === num2 ? 1 : -1;
+}
