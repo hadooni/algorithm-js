b37c97d2c5cd61d8ffc64b6316a4567f7770ab92
hadooni <hayoung7259@gmail.com>
2024-08-13 10:07:13 +0900

    solve : 두 정수 사이의 합

---
+++ Level0/22_두정수사이의합.js
@@ -0,0 +1,27 @@
+// Solution 1
+function solution(a, b) {
+  var answer = 0;
+  // a가 b보다 클 경우(a, b가 같을 경우는 둘 중 아무 수나 리턴)
+  if (a >= b) {
+    for (let i = b; i <= a; i++) {
+      // b에서 a까지 더하기
+      answer = answer += i;
+    }
+    //b가 a보다 클 경우
+  } else if (a < b) {
+    for (let i = a; i <= b; i++) {
+      // a에서 b까지 더하기
+      answer = answer += i;
+    }
+  }
+  return answer;
+}
+
+// Solution 2
+function solution(a, b) {
+  let answer = 0;
+  for (let i = Math.min(a, b); i <= Math.max(a, b); i++) {
+    answer = answer + i;
+  }
+  return answer;
+}
