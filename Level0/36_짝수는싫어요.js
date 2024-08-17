9dc44e9ae1fb29e172b3ddad383708ca28bf8bf4
hadooni <hayoung7259@gmail.com>
2024-08-17 17:55:15 +0900

    solve : 짝수는 싫어요

---
+++ Level0/36_짝수는싫어요.js
@@ -0,0 +1,17 @@
+// Solution 1
+function solution(n) {
+  var answer = [];
+  for (let i = 1; i <= n; i++) {
+    if (i % 2 === 1) {
+      answer.push(i);
+    }
+  }
+  return answer;
+}
+
+// Solution 2
+function solution(n) {
+  var answer = [];
+  for (let i = 1; i <= n; i += 2) answer.push(i);
+  return answer;
+}
